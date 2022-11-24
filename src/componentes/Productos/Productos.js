1e97e461c5d83dedbde0080add91e739f4574ebc
pink <pink@example.com>
2022-11-24 18:35:16 -0300

    formulario

--- src/componentes/Productos/Productos.js
+++ src/componentes/Productos/Productos.js
@@ -5,6 +5,7 @@ import Producto from "../Productos/Producto";
 
 
 
+
 const Productos = () => {
   
     const [Productos] = useState([
--- src/componentes/contacto/contacto.js
+++ src/componentes/contacto/contacto.js
@@ -4,20 +4,26 @@ import "./contacto.css"
 import Footer from "../footer/footer"
 
 const Contacto = () => {
-    const { register, handleSubmit } = useForm("");
+    const { register, formState:{errors} ,handleSubmit } = useForm("");
     const onSubmit = (data) =>{
         console.log(data);
     }
 
     return(
-        <div>
+        <div className="form">
             <h1>Contacto</h1>
             <h3> Para ponerse en contacto con nosotros complete el siguiente formulario</h3>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form className="formEstilo" onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <label>Nombre</label>
                 <input type="text" {...register('nombre',{required: true,
                 maxLength:10})} />
+
+                {errors.nombre?.type==='required' && <p>El campo nombre es obligatorio</p>}
+
+                {errors.nombre?.type==='maxlength' && <p>El campo nombre debe tener menos de 10 caracteres</p>}
+
+
             </div>
             <div>
                 <label>Apellido</label>
@@ -29,10 +35,19 @@ const Contacto = () => {
                 <input type="text" {...register('email',{
                     pattern: /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
                 })} />
+
+            {errors.email?.type==='pattern' && <p>El formato es incorrecto</p>}
+
             </div>
             <div>
                 <label>Telefono</label>
-                <input type="number" />
+                <input type="number"{...register('telefono',{required: true,
+                maxLength:10})} />
+
+{errors.telefono?.type==='required' && <p>El campo telefono es obligatorio</p>}
+
+{errors.telefono?.type==='maxlength' && <p>El campo telefono debe tener menos de 10 caracteres</p>}
+
             </div>
             <div>
                 <label>Asunto</label>
@@ -42,9 +57,9 @@ const Contacto = () => {
                     <option value="ct"> Citas </option>
                 </select>
             </div>
-            <input type="Submit" value="Enviar" />
+            <input type="Submit" value="Enviar" className="botonEnviar"/>
         </form>
-            <Footer/>
+            {/* <Footer/> */}
         </div>
     )
 }
