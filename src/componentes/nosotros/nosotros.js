d8ed3961b8feb891e5d5500b02a38648609bfeee
pink <pink@example.com>
2022-11-26 14:05:49 -0300

    carrusel

--- src/componentes/nosotros/nosotros.js
+++ src/componentes/nosotros/nosotros.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "../nosotros/nosotros.css"
 import Footer from "../footer/footer"
-import jacuzzi from "../img/jacuzzi.jpeg"
+import Carrusel from "../nosotros/carrusel"
 
 
 const nosotros = () => {
@@ -12,14 +12,12 @@ const nosotros = () => {
             <p>Somos una empresa familar que se dedica a la venta de baldosas atermicas hace 11 años, nuestras placas son totalmente atermicas no tienen cemento debajo. Por eso su excelente calidad. La importancia de la placa atermica es que, no se calienta con el sol, no quema los pies al pisarla. No se desgranan nuestras placas. Se puede usar en Terrazas queda muy bien y no quemaría los pies. Debido a eso la casa también quedaría más fría. Solo se debe tener en cuenta si el techo soporta el peso ya que cada una pesa 7.3 kilos. 
                 </p>
             </div>
-            {/* agegar un carrosuel para que vayan pasando las fotos */}
-            <div className="contenedorImagen">
-            <img src={jacuzzi} className="jacuzzi" alt="jacuzzi"/>
-            
+            <div >
+            <Carrusel/>
             <Footer/>
             </div>
         </header>
-
+        
     )
 }
 export default nosotros
