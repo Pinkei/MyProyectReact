8ada25eab2dfd2952945b09b86732d3e9dc37d4f
pink <pink@example.com>
2022-11-28 18:11:33 -0300

    carrusel cambios

--- src/componentes/nosotros/carrusel.js
+++ src/componentes/nosotros/carrusel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import "./carrusel.css"
 import casaPileta from "../img/casaPileta.jpg"
 import pile from "../img/pile.jpeg"
 import bordeate from "../img/bordeate.jpg"
@@ -9,38 +10,40 @@ function CarouselFadeExample() {
       <Carousel fade>
         <Carousel.Item>
           <img
-            className="casaNosotros"
+            className="contenedorImgWe"
             src={casaPileta}
-            alt="First slide"
+            alt="primera foto"
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>DECK ATERMICO</h3>
+            <p className='contenedorTextoWe'>Novedoso piso de listones realizados en hormigón con alma de hierro en su interior. Tienen todas las propiedades del piso atérmico y antideslizante.Su colocación y corte es similar a cualquier otro piso cementicio.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
+            className="contenedorImgWe"
             src={pile}
-            alt="Second slide"
+            alt="segunda foto"
           />
   
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>BORDES ATERMICOS</h3>
+            <p className='contenedorTextoWe'>Un sistema modular que se adapta según formatos y requerimientos.
+Su constitución lo hace un producto atérmico, el cual es ideal ya que su superficie no levanta temperatura.
+La porosidad de su textura previene deslizamientos brindando mayor seguridad respecto a otros materiales.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="d-block w-100"
+            className="contenedorImgWe"
             src={bordeate}
-            alt="Third slide"
+            alt="Tercera foto"
           />
   
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            <h3>AQUAVATIONS</h3>
+            <p className='contenedorTextoWe'>
+            Es un revolucionario revestimiento in situ, elegante, resistente y suave. Compuesto por cemento Portland, cuarzo y mármol pulido, es un revestimiento profesional que hará que jamás piense en pintar su pileta.
             </p>
           </Carousel.Caption>
         </Carousel.Item>
